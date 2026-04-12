f9d5e778683141df36576e01600804dc70daff47
evee00011 <evee0001@student.monash.edu>
2026-04-12 13:05:37 +0800

    Login page

--- frontend/notes-app/src/pages/Login/Login.jsx
+++ frontend/notes-app/src/pages/Login/Login.jsx
@@ -25,7 +25,7 @@ const Login = () => {
     setError("");
 
     //Login API call
-    
+
   };
 
   return (
@@ -33,7 +33,6 @@ const Login = () => {
       <Navbar />
       <div className="flex items-center justify-center mt-28"> 
         <div className="w-96 border rounded bg-white px-7 py-10">
-          {/* FIX: Point the form to handleLogin */}
           <form onSubmit={handleLogin}>
             <h4 className="text-2xl mb-7">Login</h4>
 
