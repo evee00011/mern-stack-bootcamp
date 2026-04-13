a60295b2bc8379fb68f91b1f91d899b2127a6686
evee00011 <evee0001@student.monash.edu>
2026-04-13 09:31:52 +0800

    Add button to add notes

--- frontend/notes-app/src/pages/Home/Home.jsx
+++ frontend/notes-app/src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from '../../components/Navbar/Navbar';
 import NoteCard from '../../components/Cards/NoteCard';
+import {MdAdd} from "react-icons/md"
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
       <Navbar />
       
       <div className="container mx-auto px-6">
+        <div className="grid grid-cols-3 gap-4 mt-8">
           <NoteCard title="Meeting on 7th April" 
           date="3rd Apr 2024" 
           content="Meeting on 7th April Meeting on 7th April" 
@@ -15,9 +17,14 @@ const Home = () => {
           isPinned={true}
           onEdit={()=>{}}
           onDelete={()=>{}}
-          onPinNote={()=>{}}/>
-
+          onPinNote={()=>{}}
+          />
+          
+        </div>
       </div>
+      <button className="w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10" onClick={()=>{}}>
+      <MdAdd className="text-[32px] text-white"/>
+      </button>
     </>
   );
 };
