02c63e431dce2fec66fffbeb08ed06ebb9286108
evee00011 <evee0001@student.monash.edu>
2026-04-13 08:51:19 +0800

    Note card and pin

--- frontend/notes-app/src/pages/Home/Home.jsx
+++ frontend/notes-app/src/pages/Home/Home.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
-import Navbar from '../../components/Navbar/Navbar'
+import React from 'react';
+import Navbar from '../../components/Navbar/Navbar';
+import NoteCard from '../../components/Cards/NoteCard';
 
 const Home = () => {
   return (
     <>
-    <Navbar />
-    </>
-  )
-}
+      <Navbar />
+      
+      <div className="container mx-auto px-6">
+          <NoteCard title="Meeting on 7th April" 
+          date="3rd Apr 2024" 
+          content="Meeting on 7th April Meeting on 7th April" 
+          tags ="#Meeting"
+          isPinned={true}
+          onEdit={()=>{}}
+          onDelete={()=>{}}
+          onPinNote={()=>{}}/>
 
-export default Home
+      </div>
+    </>
+  );
+};
+
+export default Home;
