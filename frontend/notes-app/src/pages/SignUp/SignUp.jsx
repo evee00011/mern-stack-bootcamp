01eb6a9ffdb8f1fdb625093b7589126fc07a0462
evee00011 <evee0001@student.monash.edu>
2026-04-14 12:22:01 +0800

    Handle create account data

--- frontend/notes-app/src/pages/SignUp/SignUp.jsx
+++ frontend/notes-app/src/pages/SignUp/SignUp.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom'; // Added
+import { Link, useNavigate } from 'react-router-dom'; 
 import Navbar from "../../components/Navbar/Navbar";
-import PasswordInput from "../../components/Input/PasswordInput"; // Added
-import { validateEmail } from "../../utils/helper"; // Added
+import PasswordInput from "../../components/Input/PasswordInput"; 
+import { validateEmail } from "../../utils/helper"; 
+import axiosInstance from '../../utils/axiosInstance';
 
 const SignUp = () => {
   const [name, setName] = useState("");
@@ -10,6 +11,7 @@ const SignUp = () => {
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
 
+  const navigate = useNavigate()
   const handleSignUp = async (e) => {
     e.preventDefault();
 
@@ -31,7 +33,31 @@ const SignUp = () => {
     setError("");
 
     // SignUp API Call Logic
-    console.log("Signing up:", { name, email, password });
+    try {
+      const response = await axiosInstance.post('/create-account', {
+        fullName:name,
+        email: email,
+        password: password,
+      });
+
+      // handle unsuccessful register response
+      if (response.data && response.data.error) {
+        setError(response.data.message)
+        return
+      }
+      
+      if (response.data && response.data.accessToken) {
+        localStorage.setItem('token', response.data.accessToken);
+        navigate('/dashboard');
+      }
+    } catch (error) {
+      
+      if (error.response && error.response.data && error.response.data.message) {
+        setError(error.response.data.message);
+      } else {
+        setError('An unexpected error occured. Please try again.')
+      }
+    }
   };
 
   return (
